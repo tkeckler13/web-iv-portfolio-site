169b1faa665d9246d72f476868975defbed2221e
tkeckler13 <tkeckler13@yahoo.com>
2015-06-02 22:27:53 -0500

    June 2 Commit 3
    Contact Form Works!!
    moved around contact info
    replaced images with smaller sizes

---
+++ contact-engine.php
@@ -0,0 +1,43 @@
+<?php
+
+$EmailFrom = "from@mydomain.com";
+$EmailTo = "taylor@teaspoondesign.com";
+$Subject = "Contact Form";
+$Name = Trim(stripslashes($_POST['Name'])); 
+$Email = Trim(stripslashes($_POST['Email'])); 
+$Phone = Trim(stripslashes($_POST['Phone'])); 
+$Message = Trim(stripslashes($_POST['Message'])); 
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Tel: ";
+$Body .= $Tel;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=contact-thanks.php\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+}
+?>
---
+++ contact-thanks.php
@@ -0,0 +1,86 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+<title>Thank You!</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+<link rel="stylesheet" type="text/css" href="styles/foundation.css">
+<link rel="stylesheet" type="text/css" href="styles/custom.css">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+
+
+<section id="navigation">
+    	<div class="logo large-2 medium-3 small-4 columns end"><img src="images/logo-2.png" alt="logo"></div>
+    		<div class="container">
+				<nav id="navigation">
+					<div class="nav-trigger">Menu<span></span></div>
+					<ul class="menu">
+						<li><a href="index.html">Home</a></li>
+						<li><a href="index.html#work">Work</a></li>
+						<li><a href="index.html#about">About</a></li>
+						<li><a href="index.html#contact">Contact</a></li>
+					</ul>
+				</nav>
+			</div>
+    </section>
+    
+    <section id="social-icons">
+    		<div class="social-column large-1 medium-1 small-2 columns end">
+                	<div class="row social">
+                       	<a href="https://www.facebook.com/tkeckler">
+            				<img src="images/facebook.png" width="50" alt="icon">
+                        </a>
+                   	</div>
+                   	<div class="row social">
+                    	<a href="https://www.behance.net/teaspoondesign">
+    						<img src="images/behanced.png" width="50" alt="icon">
+                        </a>
+                   	</div>
+                   	<div class="row social">
+                    	<a href="https://instagram.com/tkeckler13/">
+    						<img src="images/instagram.png" width="50" alt="icon">
+                        </a>
+                   	</div>
+                   	<div class="row social">
+                    	<a href="https://twitter.com/Taytertitss?lang=en">
+	         	  			<img src="images/twitter.png" width="50" alt="icon">
+                        </a>
+                   	</div>                    	
+                    <div class="row social">
+                    	<a href="https://dribbble.com/teaspoondesign">
+                			<img src="images/dribbble.png" width="50" alt="icon">
+                        </a>
+                    </div>
+                   	<div class="row social">
+                    	<a href="https://www.linkedin.com/profile/preview?vpa=pub&amp;locale=en_US">
+          	  				<img src="images/linked.png" width="50" alt="icon">
+                        </a>
+                   	</div>
+    		</div>
+    </section>
+    
+    
+	<div id="thank-you">
+    	<div class="row">
+			<div class="medium-6 small-8 column end">
+				<img src="images/drawn-heading-thanks.png">
+        	</div>
+        </div>
+			
+			
+		<br /><br />
+		
+		<div class="row">
+			<h2>Your message has been sent!</h2><br />
+        </div>
+		
+		<p><a href="index.html">Back to Home</a></p>
+	
+	</div>
+
+
+</body>
+
+</html>
